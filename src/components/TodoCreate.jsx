6e2fffdcd8b8f6b3d30624538d11a381b2ebe3fb
heywon0909 <hw7319@naver.com>
2023-01-10 22:48:06 +0900

    todo 아이템 localStorage에 넣기

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../css/todoCreate.module.css';
 
 export default function TodoCreate() {
+   const [todoItem,setTodoItem] = useState('')
     return (
         <div className={styles.container}>
-            <input type="text" placeholder='Add ToDo' onInput={(e)=>console.log(e.target.value)}/>
-            <button className={styles.addBtn}>add</button>
+            <input type="text" placeholder='Add ToDo' onChange={(e)=>setTodoItem(e.target.value)}/>
+            <button className={styles.addBtn} onClick={() => {
+                if (!localStorage.todo) {
+                    localStorage.todo = [todoItem];
+                } else {
+                    let arr = [localStorage.todo,todoItem];
+                    localStorage.todo = arr;
+                }
+            }}>add</button>
         </div>
     );
 }
