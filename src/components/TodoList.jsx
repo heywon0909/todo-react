eaa9e34bdc07054d1658908143a0b04fdb42b685
heywon0909 <hw7319@naver.com>
2023-01-10 22:22:59 +0900

    list component 생성

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import styles from '../css/todoList.module.css'
+import {RiDeleteBin2Fill} from 'react-icons/ri'
 
 export default function TodoList() {
     return (
         <div>
             <ul>
                 <li>
-                    <input type='checkbox'/>
+                <div className={styles.todoItem}>
+                    <input type='checkbox' />
+                    공부하기
+                    <div className={styles.deleteBtn}>
+                            <RiDeleteBin2Fill className={styles.icon} />
+                    </div>
+                    </div>
                 </li>
             </ul> 
         </div>
